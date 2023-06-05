caa42bbd2109e864af901fd5560c7c25d67a42b9
GallyamovAmir <amirgallyamov9967@gmail.com>
2023-06-05 20:38:39 +0300

    Brought all windows to a common style and cleaned up the code a bit

--- MyTrain/FormalizedTicketPage.xaml.cs
+++ MyTrain/FormalizedTicketPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MyTrain
 {
@@ -77,8 +66,9 @@ namespace MyTrain
             if (selectedTicketId == 0)
                 return;
 
-            if (int.Parse(TBUserId.Text) == 0)
+            if ( string.IsNullOrEmpty(TBUserId.Text) || int.Parse(TBUserId.Text) == 0)
                 return;
+
 
             var db = new MyTrainEntities();
 
